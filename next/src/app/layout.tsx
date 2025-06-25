fb72f2a9b0fcfd0d01a42a918fa76c11f7c6675e
cheick <cheicklanick@gmail.com>
2025-06-25 20:55:41 +0200

    Search page

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+     <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-default-gradient`}
       >
+		<Header />
         {children}
+		<Footer />
       </body>
     </html>
   );
